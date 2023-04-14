34d5f36fdb8803ef134a527328c329d252d569fc
Gilang Heavy <gilangheavy@gmail.com>
2023-04-14 22:49:15 +0700

    create form and post route

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const app = express();
 const port = 3000;
 todos.init();
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index", {
     title: "Todolist With NodeJS",
     todos: todos.showTodo(),
   });
+});
+app.post("/", (req, res) => {
+  todos.insertTodo(req.body.action);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
